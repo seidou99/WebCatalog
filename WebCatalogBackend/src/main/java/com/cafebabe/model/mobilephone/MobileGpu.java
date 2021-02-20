c4b9bbdbfeffd7af88ac0e8e7c891defae4bd811
Pavel.Maryanski <mrskypavlik@gmail.com>
2021-02-20 13:45:08 +0300

    started backend

---
+++ WebCatalogBackend/src/main/java/com/cafebabe/model/mobilephone/MobileGpu.java
@@ -0,0 +1,9 @@
+package com.cafebabe.model.mobilephone;
+
+import com.cafebabe.model.BaseDataObject;
+
+import javax.persistence.Entity;
+
+@Entity
+public class MobileGpu extends BaseDataObject {
+}
